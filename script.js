297c084e898b88e15caf037ea2e8e8cac4650fd3
phantomramen <ali.hernandez9two@gmail.com>
2020-08-04 23:18:26 -0600

    push

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ $('.fixed-action-btn').floatingActionButton({
   });
   
 
-  
+  //web audio api test
+ 
+const audioCtx = new AudioContext();
+const osc = audioCtx.createOscillator();
+const play = document.getElementById("play");
+
+
+osc.type = "square";
+osc.frequency.setValueAtTime(400, audioCtx.currentTime);
+
+function startOsc(){
+  osc.connect(audioCtx.destination);
+  osc.start();
+  osc.stop(audioCtx.currentTime + .5);
+}
+play.addEventListener('click', startOsc);
+
 
--- tone.js
+++ tone.js
@@ -1,12 +1,12 @@
-const osc = new Tone.Oscillator().toDestination();
-// start at "C4"
-osc.frequency.value = "C4";
-// ramp to "C2" over 2 seconds
-osc.frequency.rampTo("C2", 2);
-// start the oscillator for 2 seconds
-osc.start().stop("+3");
-//attach a click listener to a play button
-document.querySelector('.dropdown-trigger').addEventListener('click', async () => {
-  await Tone.start()
-  console.log('audio is ready')
-})
+// const osc = new Tone.Oscillator().toDestination();
+// // start at "C4"
+// osc.frequency.value = "C4";
+// // ramp to "C2" over 2 seconds
+// osc.frequency.rampTo("C2", 2);
+// // start the oscillator for 2 seconds
+// osc.start().stop("+3");
+// //attach a click listener to a play button
+// document.querySelector('.dropdown-trigger').addEventListener('click', async () => {
+//   await Tone.start()
+//   console.log('audio is ready')
+// })
