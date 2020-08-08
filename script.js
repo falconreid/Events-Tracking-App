59d5c722e5d5af309c025087b65ced2967d07268
phantomramen <ali.hernandez9two@gmail.com>
2020-08-08 11:08:54 -0600

    added meetup tone

--- script.js
+++ script.js
@@ -11,8 +11,19 @@ play.addEventListener("click", () => {
   const ctx = new (window.AudioContext || window.webkitAudioContext)();
 
   const osc = ctx.createOscillator();
+  const amp = ctx.createGain();
 
-  osc.connect(ctx.destination);
+  osc.frequency.value = 200;
+  osc.connect(amp.gain);
+
+  const filter = ctx.createBiquadFilter();
+
+  filter.frequency.value = 250;
+
+  osc.connect(filter);
+  filter.connect(ctx.destination);
+
+  //osc.connect(ctx.destination);
   osc.start(0);
   osc.stop(0.5);
   osc.onended = () => {
@@ -21,6 +32,48 @@ play.addEventListener("click", () => {
 });
 
 //meetup tone
+
+const meet = document.getElementById("meet");
+meet.addEventListener("click", () => {
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+  const osc = ctx.createOscillator();
+  const amp = ctx.createGain();
+
+  osc.frequency.value = 250;
+  osc.connect(amp.gain);
+
+  const filter = ctx.createBiquadFilter();
+
+  filter.frequency.value = 450;
+
+  osc.connect(filter);
+  filter.connect(ctx.destination);
+
+  //osc.connect(ctx.destination);
+  osc.start(0);
+  osc.stop(0.5);
+  osc.onended = () => {
+    console.log(ctx.state);
+  };
+});
+
+
+// const play = document.getElementById("play");
+// play.addEventListener("click", () => {
+// const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+// const audioElement = document.querySelector('audio');
+
+// const mediaElement = ctx.createMediaElementSource(audioElement);
+
+// mediaElement.connect(ctx.destination);
+  
+//   audio.start(0);
+//   audio.stop(0.5);
+
+//});
+
 
 //pull user events
 //append user events to list or calender view
